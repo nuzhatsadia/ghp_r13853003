ede479298543070244e4ff1cde3654f5c83f83e0
nuzhatsadia <r13853003@ntu.edu.tw>
2025-05-22 16:03:20 +0800

    imported dengue dataset and calculated cases in 2014

--- HRM_assignment.R
+++ HRM_assignment.R
@@ -1,6 +1,5 @@
-<<<<<<< Updated upstream
-created a R script for assignment
-=======
+#created a R script for assignment
+
 setwd("C:/Users/nuzha/R-spring 2025/R-course")
 #import the dataset
 df<-read.csv(file = "dengue_assignment.csv", header = TRUE)
@@ -9,4 +8,3 @@ df<-read.csv(file = "dengue_assignment.csv", header = TRUE)
 dengue_2014 <- sum(df$case_number[df$year == 2014])
 print(dengue_2014)
 #so total case in the year 2014 was 15747
->>>>>>> Stashed changes
